e5856d3c89a23e68baa00a5a481f99d990dffe98
Taylor Pourtaheri <pourtaheri@hemoshear.com>
2021-04-23 11:01:37 -0400

    added new functions to score network and evaluate performance

---
+++ R/evaluate_performance.R
@@ -0,0 +1,41 @@
+evaluate_performance <- function(network, network_df, causal_sim,
+                                 method, n_sim, weighted = FALSE){
+
+    if (weighted == FALSE){
+
+        mean_score <- mean(network_df$causal_similarity)
+
+        # estimate uncertainty with a random draw of the full ppi graph
+        n_draws <- length(igraph::V(network))
+        samples <- lapply(1:n_sim, function(x) sample(causal_sim, n_draws))
+
+        sample_means <- sapply(samples, mean)
+
+        # calculate p
+        score_pval <- sum(sample_means > mean_score) / n_sim
+
+    }
+
+    if (weighted == TRUE){
+
+        mean_score <- mean(network_df$weighted_score)
+        weights <- network_df[,method]
+
+        # estimate uncertainty with a random draw of the full ppi graph
+        n_draws <- length(igraph::V(network))
+        samples <- lapply(1:n_sim, function(x) sample(causal_sim, n_draws))
+
+        weighted_samples <- purrr::map(samples, ~ .*weights)
+        sample_means <- sapply(weighted_samples, mean)
+
+        # calculate p
+        score_pval <- sum(sample_means > mean_score) / n_sim
+
+    }
+
+    output_df <- data.frame('mean_score' = mean_score,
+                            'score_pval' = score_pval)
+
+    return(output_df)
+
+}
---
+++ R/structural_sim.R
@@ -0,0 +1,53 @@
+structural_sim <- function(network, string_db, ppi, method, sim_method, causal_gene_symbol, weighted = FALSE){
+
+    output_list <- c()
+
+    # dataframe of final graph results
+    network_df <- igraph::as_data_frame(network, what = 'vertices')
+    rownames(network_df) <- NULL
+
+    # find the STRING ID for the causal gene
+
+    xref <- data.frame(symbol = causal_gene_symbol)
+    xref <- string_db$map(xref, "symbol", removeUnmappedRows=T, quiet=T)
+
+    # calculate similarity of each node and slice out the causal gene row
+    sim <- igraph::similarity(ppi, method = sim_method)
+    index <- which(igraph::V(ppi)$name == xref$STRING_id)
+    causal_sim <- sim[index,]
+
+    # make scores a named vector
+    names(causal_sim) <- igraph::V(ppi)$name
+
+    # get the scores associated with the subnetwork
+    pred_scores <- causal_sim[igraph::V(network)$name]
+
+    sim_scores <- data.frame('causal_similarity' = pred_scores,
+                             'name' = igraph::V(network)$name)
+    network_df <- dplyr::left_join(network_df, sim_scores)
+
+    # add vertex attributes to network
+    network <- igraph::set_vertex_attr(network, 'causal_similarity',
+                                       value = pred_scores)
+
+
+    if(weighted == TRUE){
+
+        # create final weighted scoring metric and output table
+        network_df$weighted_score <- network_df[,method]*network_df[ ,'causal_similarity']
+
+        # add vertex attribute
+        network <- igraph::set_vertex_attr(network, 'weighted_score',
+                                           value = network_df$weighted_score)
+
+        # sorted output
+        network_df <- dplyr::arrange(network_df, -weighted_score)
+    }
+
+    output_list[['network']] <- network
+    output_list[['causal_similarity']] <- causal_sim
+    output_list[['network_df']] <- network_df
+
+
+    return(output_list)
+}
