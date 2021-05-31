459029017cc02e5ecbf2d7b63d0e3c362e84cdc9
Taylor Pourtaheri <pourtaheri@hemoshear.com>
2021-05-31 15:08:56 -0400

    generalized name of output from 'diffusion_score' to 'propagation_score', to avoid conflicting with other methods

--- R/calc_diffusion.R
+++ R/calc_diffusion.R
@@ -1,22 +1,24 @@
-#' @title Network propagation
-#' @description Calculate various measures of network propagation
+#' @title Calculate network propagation by diffusion
+#' @description Calculate various measures of diffusion
 #' @import diffuStats
 #' @param graph Graph of class '\code{igraph}'. Must include node
 #' attributes '\code{name}' and '\code{seed}'.
 #' @param method String. Kernel for diffusion. Passed to \code{diffusion()}
 #' from the \code{diffuStats} package.
-#' @return Graph of class '\code{igraph}' with new attribute, '\code{diffusion_score}'.
+#' @return Graph of class '\code{igraph}' with new attribute, '\code{propagation_score}'.
 #' @export
 calc_diffusion <- function(graph, method = 'raw') {
 
     # simulate diffusion
     scores <- as.data.frame(igraph::vertex_attr(graph))$seed
     names(scores) <- as.data.frame(igraph::vertex_attr(graph))$name
+
     diffusion_scores <- diffuStats::diffuse(graph = graph,
                                             scores = scores,
                                             method = method)
+
     graph <- igraph::set_vertex_attr(graph,
-                                     name = 'diffusion_score',
+                                     name = 'propagation_score',
                                      value = diffusion_scores)
 
     return(graph)
