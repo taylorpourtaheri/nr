9551e291d812f3d1f16b8cfd1ba63caf1a19a45b
Monish Dadlani <mkd7q@virginia.edu>
2021-07-12 21:02:15 -0400

    Added download button

--- node_rank_app/app.R
+++ node_rank_app/app.R
@@ -5,7 +5,7 @@ library(DT)
 devtools::load_all()
 
 # load in the deg
-de_string <- readRDS('data/de_string_v11.RDS')
+de_string <- readRDS('de_string_v11.RDS')
 myc_de <- de_string$MYC
 
 
@@ -13,43 +13,46 @@ myc_de <- de_string$MYC
 ui <- fluidPage(
     
     theme = shinytheme("flatly"),
-
+    
     # Application title
     titlePanel(h1("Differential Gene Expression Data Analysis", align = 'center')),
-
+    
     # Sidebar 
     sidebarLayout(
         sidebarPanel(width = 3,
-            textInput(inputId = 'target', label = 'Enter the target gene'),
-            selectInput(inputId = 'method', label = 'Centrality method', choices = c('avg_strength',
-                                                                                     'betweenness',
-                                                                                     'degree',
-                                                                                     'evcent_w',
-                                                                                     'strength',
-                                                                                     'evcent_uw')),
-            selectInput(inputId = 'logFC', label = 'Log fold change filter', choices = c(0.0, 0.5, 1.0, 1.5, 2.0)),
-            selectInput(inputId = 'pvalue', label = 'P-value filter', choices = c(0.05, 0.10, 0.15, 0.20, 0.25)),
-            selectInput(inputId = 'connected', label = 'Connected subgraph filter', choices = c("TRUE"=1,"FALSE"=0)),
-            selectInput(inputId = 'weighted', label = 'Weighted scoring', choices = c("TRUE"=1,"FALSE"=0)),
-            actionButton(inputId = "build", label = 'Build network')
-            )
+                     textInput(inputId = 'target', label = 'Enter the target gene'),
+                     selectInput(inputId = 'method', label = 'Centrality method', choices = c('avg_strength',
+                                                                                              'betweenness',
+                                                                                              'degree',
+                                                                                              'evcent_w',
+                                                                                              'strength',
+                                                                                              'evcent_uw')),
+                     selectInput(inputId = 'logFC', label = 'Log fold change filter', choices = c(0.0, 0.5, 1.0, 1.5, 2.0)),
+                     selectInput(inputId = 'pvalue', label = 'P-value filter', choices = c(0.05, 0.10, 0.15, 0.20, 0.25)),
+                     selectInput(inputId = 'connected', label = 'Connected subgraph filter', choices = c("TRUE"=1,"FALSE"=0)),
+                     selectInput(inputId = 'weighted', label = 'Weighted scoring', choices = c("TRUE"=1,"FALSE"=0)),
+                     actionButton(inputId = "build", label = 'Build network')
+        )
         ,
-
+        
         mainPanel(
             width = 9,
             navbarPage(title = 'Outputs:',
                        tabPanel('Network Plot',plotOutput('nodePlot')),
                        tabPanel('Target Results',DT::dataTableOutput('targetPerformance')),
-                       tabPanel('Table of Genes',DT::dataTableOutput('topGenes'))
+                       tabPanel('Table of Genes',DT::dataTableOutput('topGenes')),
+                       selectInput("dataset", "Choose a dataset:",
+                                   choices = c("Target Results","Table of Genes")),
+                       downloadButton("downloadData", "Download")
             )
-
+            
         )
     )
 )
 
 # Define server logic 
 server <- function(input, output) {
-
+    
     observeEvent(input$build, {
         results <- noderank::centrality_pipeline(deg = myc_de,
                                                  edge_conf_score_min = 950,
@@ -65,11 +68,11 @@ server <- function(input, output) {
         
         output$nodePlot <- renderPlot({
             plot_graph(results[['network']], method = 'weighted_score', gene_list = c(input$target))
-            })
+        })
         
         
         performanceNames <- c(names(results[['performance']])[-6])
-            
+        
         output$targetPerformance <- DT::renderDataTable({datatable(results[['performance']]) %>%
                 formatRound(performanceNames, 3)})
         
